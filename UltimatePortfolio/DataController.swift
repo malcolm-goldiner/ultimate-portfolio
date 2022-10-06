802695b1c17b5348b925cecc757b100c455a68b8
Malcolm Goldiner <52385034+malcolm-goldiner@users.noreply.github.com>
2022-10-05 21:32:40 -0700

    Add DataController

--- UltimatePortfolio/DataController.swift
+++ UltimatePortfolio/DataController.swift
@@ -7,3 +7,81 @@
 
 import CoreData
 import SwiftUI
+
+// SwiftUI objects can create instances of it
+class DataController: ObservableObject {
+    // handles syncing/fetching of coredata entires in ck for this user
+    let container: NSPersistentCloudKitContainer
+    
+    init(inMemory: Bool = false) {
+        self.container = NSPersistentCloudKitContainer(name: "main")
+        
+        if inMemory {
+            // in memory caches are nice for testing
+            container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath:"/dev/null")
+        }
+        
+        container.loadPersistentStores { storeDescription, error in
+            if let error = error {
+                fatalError("Fatal error loading sotre: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    static var preview: DataController = {
+        let dataController = DataController(inMemory: true)
+        let viewContext = dataController.container.viewContext
+        do {
+            try dataController.createSampleData()
+        } catch {
+            fatalError("Fatal error creating preview: \(error.localizedDescription)")
+        }
+        
+        return dataController
+    }()
+    
+    func createSampleData() throws {
+        // Pool of data loaded to disk, active object.
+        let viewContext = container.viewContext
+        
+        for i in 1...5 {
+            let project = Project(context: viewContext)
+            project.title = "Project \(i)"
+            project.items = []
+            project.creationDate = Date()
+            project.closed = Bool.random()
+            
+            for j in 1...10 {
+                let item = Item(context: viewContext)
+                item.title = "Item \(j)"
+                item.completed = Bool.random()
+                item.creationDate = Date()
+                item.project = project
+                item.priority = Int16.random(in: 1...3)
+            }
+        }
+        
+        // write to permanent storage or memory
+        try viewContext.save()
+    }
+    
+    func save() {
+        if container.viewContext.hasChanges {
+            try? container.viewContext.save()
+        }
+    }
+    
+    func delete(_ object: NSManagedObject) {
+        container.viewContext.delete(object)
+    }
+    
+    func deleteAll() {
+        let fetchRequest1: NSFetchRequest<NSFetchRequestResult> = Item.fetchRequest()
+        let batchDeleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest1)
+        _ = try? container.viewContext.execute(batchDeleteRequest)
+        
+        let fetchRequest2: NSFetchRequest<NSFetchRequestResult> = Project.fetchRequest()
+        let batchDeleteRequest2 = NSBatchDeleteRequest(fetchRequest: fetchRequest2)
+        _ = try? container.viewContext.execute(batchDeleteRequest2)
+    }
+}
