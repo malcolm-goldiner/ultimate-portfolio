e11079a44d973a37685a85b2471effb639625cd9
Malcolm Goldiner <support@watchcallerapp.com>
2022-10-05 20:42:14 -0700

    Adding base project.

---
+++ UltimatePortfolio/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  UltimatePortfolio
+//
+//  Created by Malcolm Goldiner on 10/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Malcolm Goldiner on 10/5/22.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
